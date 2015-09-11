b90483a0da35a7fd49b387dc13b26af99ecf22b4
Travis Montgomery <travisbm@gmail.com>
2015-09-11 16:36:59 -0500

    migrated AddTaskTable

--- db/migrate/20150911163050_add_task_table.rb
+++ db/migrate/20150911163050_add_task_table.rb
@@ -1,6 +1,6 @@
 class AddTaskTable < ActiveRecord::Migration
   def change
-    creat_table(:tasks) do |t|
+    create_table(:tasks) do |t|
       t.string  :task, limit:50
       t.boolean :complete, default: TRUE
       t.timestamps
